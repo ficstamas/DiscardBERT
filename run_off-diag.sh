6dee688af71d4e43602f41f3bbe8c58d03c10536
Tamás Ficsor <ficstamas@gmail.com>
2023-09-09 21:06:56 +0900

    Changed wandb project

--- run_off-diag.sh
+++ run_off-diag.sh
@@ -5,8 +5,8 @@ for model in ${models[@]}
 do
 for seed in ${seeds[@]}
 do
-python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "cola" --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "matthews_correlation" --recursive_steps "off_diagonal"
-python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "mrpc" --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "f1" --recursive_steps "off_diagonal"
-python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "rte"  --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "accuracy" --recursive_steps "off_diagonal"
+python cli.py --wandb_project "recursive" --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "cola" --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "matthews_correlation" --recursive_steps "off_diagonal"
+python cli.py --wandb_project "recursive" --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "mrpc" --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "f1" --recursive_steps "off_diagonal"
+python cli.py --wandb_project "recursive" --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "rte"  --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "accuracy" --recursive_steps "off_diagonal"
 done
 done
