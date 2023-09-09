c2d509349483a9e94be4a90fdbf48fe7d07aed9a
Tamás Ficsor <ficstamas@gmail.com>
2023-09-09 14:35:37 +0900

    fixed tokenizers and included conll script

--- run_off-diag_conll.sh
+++ run_off-diag_conll.sh
@@ -5,8 +5,7 @@ for model in ${models[@]}
 do
 for seed in ${seeds[@]}
 do
-python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "cola" --tokenizer_name "bert-base-uncased" --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "matthews_correlation" --recursive_steps "off_diagonal"
-python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "mrpc" --tokenizer_name "bert-base-uncased" --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "f1" --recursive_steps "off_diagonal"
-python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --subset_name "rte"  --tokenizer_name "bert-base-uncased" --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "accuracy" --recursive_steps "off_diagonal"
+python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --dataset_name "conll" --subset_name "ner" --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "f1" --recursive_steps "off_diagonal"
+python cli.py --seed $seed --batch_size 16 --initial_model "pfdf" --device "cuda" --model_name $model --dataset_name "conll" --subset_name "pos" --tokenizer_name $model --tokenizer_truncation "longest_first" --tokenizer_max_length 256 --use_wandb recursive --target_metrics "f1" --recursive_steps "off_diagonal"
 done
 done
