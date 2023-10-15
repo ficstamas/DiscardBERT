f40b54f9b5781d7c59428bf691dddfbd9340a7c9
Tamás Ficsor <ficstamas@gmail.com>
2023-10-15 11:51:06 +0000

    run scripts

--- run_distil_lrec.sh
+++ run_distil_lrec.sh
@@ -6,9 +6,15 @@ for task in ${tasks[@]}
 do
 for init in ${inits[@]}
 do
-for seed in `seq 0 1 4`
+for from in `seq 0 2 4`
+do
+for to in `seq ${i+2} 2 4`
+do
+for seed in `seq 1 1 4`
 do
 	python cli.py --model_name "distilbert-base-uncased" --tokenizer_name "distilbert-base-uncased" --subset_name $task --initial_model $init --use_wandb --wandb_project "lrec" --range "0" "0" --seed $seed --elimination "range" --device "cuda" --tokenizer_truncation "longest_first" --tokenizer_max_length 256  simple
+done
+done
 done
 done
 done
---
+++ run_distil_lrec_conll.sh
@@ -0,0 +1,14 @@
+tasks=("ner" "pos")
+inits=("pdf")
+
+
+for task in ${tasks[@]}
+do
+for init in ${inits[@]}
+do
+for seed in `seq 0 1 4`
+do
+	python cli.py --model_name "distilbert-base-uncased" --tokenizer_name "distilbert-base-uncased" --model_type "token" --dataset_name "conll" --subset_name $task --initial_model $init --use_wandb --wandb_project "lrec" --range "0" "0" --seed $seed --elimination "range" --device "cuda" --tokenizer_truncation "longest_first" --tokenizer_max_length 256  simple
+done
+done
+done
