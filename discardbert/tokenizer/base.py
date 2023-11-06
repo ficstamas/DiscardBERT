d380212584ea97376196bef82120cc14b120e58c
Tamás Ficsor <ficstamas@gmail.com>
2023-11-06 14:29:58 +0100

    Fix roberta tokenizer

--- discardbert/tokenizer/base.py
+++ discardbert/tokenizer/base.py
@@ -5,7 +5,11 @@ from transformers import AutoTokenizer
 class Tokenizer(abc.ABC):
     def __init__(self, tokenizer_name, **kwargs):
         self.tokenizer_name = tokenizer_name
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+        kw = {}
+        if "add_prefix_space" in kwargs:
+            kw["add_prefix_space"] = kwargs["add_prefix_space"]
+            kwargs.pop("add_prefix_space")
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, **kw)
         self.kwargs = kwargs
 
     @abc.abstractmethod
